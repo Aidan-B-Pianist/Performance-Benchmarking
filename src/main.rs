b32ce824cd1ef525a6289b3707017b601afe630c
aburchett <aidan@burchett.me>
2025-04-14 14:19:45 -0500

    70 points total commit, need last 30 using pre-sorted input.

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,61 @@ use std::time::Instant;
 use rand::Rng;
 
 fn main() {
-    let sizes = [10, 100, 1000, 10000, 100000];
+    let sizes = [100000];
 
-    let k_sizes = [5, 10, 20, 35, 50];
+    let k_sizes = [10, 20, 30, 40, 50];
 
+    //Hybrid sort
     for size in sizes {
         println!("Testing array size of {}.", size);
 
         let mut rng = rand::thread_rng();
-        let array: Vec<i32> = (0..size).map(|_| rng.gen_range(0..1000000)).collect();
+        let array: Vec<i32> = (0..size).map(|_| rng.gen_range(0..100000)).collect();
 
-        for k in k_sizes {
+        //Hybrid Sort
+        println!("Hybrid Sort");
+        {
+            for k in k_sizes {
             
+                let mut test_array = array.clone();
+    
+                let start_time = Instant::now();
+                hybrid_sort(&mut test_array, k);
+                let duration = start_time.elapsed();
+    
+                let is_sorted = test_array.windows(2).all(|n| n[0] <= n[1]);
+    
+                println!("The array is sorted: {}", is_sorted);
+                println!("The array length is {}, with k-value: {}, sorted in {:?}", size, k, duration);
+            }
+        }
+        //Quick Sort
+        println!("Quick Sort");
+        {
             let mut test_array = array.clone();
-
             let start_time = Instant::now();
-            quick_hybrid_sort(&mut test_array, k);
+            let len = test_array.len();
+            quick_sort_ret(&mut test_array, 0, len as isize - 1);
             let duration = start_time.elapsed();
 
             let is_sorted = test_array.windows(2).all(|n| n[0] <= n[1]);
 
             println!("The array is sorted: {}", is_sorted);
-            println!("The array length is {}, with k-value: {}, sorted in {:?},", size, k, duration);
+            println!("The array length is {}, sorted in {:?}", size, duration);
+        }
+        //Insertion Sort
+        println!("Insertion Sort");
+        {
+            let mut test_array = array.clone();
+
+            let start_time = Instant::now();
+            insertion_sort_ret(&mut test_array);
+            let duration = start_time.elapsed();
+
+            let is_sorted = test_array.windows(2).all(|n| n[0] <= n[1]);
+
+            println!("The array is sorted: {}", is_sorted);
+            println!("The array length is {}, sorted in {:?}", size, duration);
         }
     }
 }
@@ -35,7 +68,7 @@ fn main() {
 
 // Part 1
 // This function implements a hybrid sorting algorithm that combines quicksort and insertion sort.
-fn quick_hybrid_sort<T: Ord + Clone>(arr: &mut [T], k: usize) -> &mut [T] {
+fn hybrid_sort<T: Ord + Clone>(arr: &mut [T], k: usize) -> &mut [T] {
     if arr.len() <= 1 {
         return arr;
     }
@@ -57,8 +90,10 @@ fn quick_sort<T: Ord + Clone>(arr: &mut [T], start: isize, end: isize, k: usize)
             quick_sort(arr, start, pivot - 1, k);
             quick_sort(arr, pivot + 1, end, k);
         } else {
-            insertion_sort(arr);
+            insertion_sort(arr, start, end);
         }
+    } else {
+        return;
     }
 }
 
@@ -79,8 +114,20 @@ fn partition<T: Ord + Clone>(arr: &mut [T], start: isize, end: isize) -> isize {
     return pivot_index;
 }
 
+//Insertion sort algorithm paired with quicksort
+fn insertion_sort<T: Ord>(arr: &mut [T], start: isize, end: isize) {
+    for i in (start + 1)..=end {
+        let mut j = i;
 
-fn insertion_sort<T: Ord>(arr: &mut [T]) {
+        while j > start && arr[(j - 1) as usize] > arr[j as usize] {
+            arr.swap(j as usize, (j - 1) as usize);
+            j -= 1;
+        }
+    }
+}
+
+//Insertion sort algorithm
+fn insertion_sort_ret<T: Ord>(arr: &mut [T]) {
     for i in 1..arr.len() {
 
         let mut j = i;
@@ -89,5 +136,18 @@ fn insertion_sort<T: Ord>(arr: &mut [T]) {
             arr.swap(j, j - 1);
             j -= 1;
         }
+    }
+}
+
+
+fn quick_sort_ret<T: Ord + Clone>(arr: &mut [T], start: isize, end: isize) {
+    if start <= end {
+    
+    let pivot = partition(arr, start, end);
+
+    quick_sort_ret(arr, start, pivot - 1);
+    quick_sort_ret(arr, pivot + 1, end);
+
+
     }
 }
